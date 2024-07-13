082a26d9dd6b6a925d2ef76aa33596c42e7dc3b4
Angel Dario Arreola Lucio <20151604@aguascalientes.tecnm.mx>
2024-07-13 03:47:32 -0600

    Modificación de componentes

--- src/Components/menuop.jsx
+++ src/Components/menuop.jsx
@@ -2,60 +2,32 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import styled from 'styled-components';
-
-const MenuContainer = styled.div`
-  background-color: #333;
-  padding: 20px;
-  border-radius: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-  width: 200px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const MenuItem = styled.div`
-  padding: 15px;
-  border-bottom: 1px solid #444;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  cursor: pointer;
-  :hover {
-    background-color: #444;
-  }
-`;
-
-const MenuLink = styled.a`
-  text-decoration: none;
-  color: #fff;
-  font-size: 16px;
-  font-weight: bold;
-  :hover {
-    color: #ccc;
-  }
-`;
-
-const Icon = styled.i`
-  font-size: 18px;
-  margin-left: 10px;
-  color: #fff;
-`;
-
+import React from 'react';
+import { Navbar as RNavbar, Nav as RNav, NavItem as RNavItem, NavLink as RNavLink} from 'react-bootstrap';
 function MenuOpciones() {
   return (
-    <MenuContainer>
-      <MenuItem>
-        <MenuLink href="#">Órdenes de compra</MenuLink>
-        <Icon className="fas fa-shopping-cart" />
-      </MenuItem>
-      <MenuItem>
-        <MenuLink href="#">Revisión de reseñas de productos</MenuLink>
-        <Icon className="fas fa-star" />
-      </MenuItem>
-    </MenuContainer>
+    <RNavbar fixed="top" bg="light" expand="lg">
+      <Navbar.Brand href="#home">Mi Aplicación</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <RNav className="mr-auto">
+          <RNavItem>
+            <RNavLink href="#" className="nav-link">
+              Órdenes de compra
+              <i className="fas fa-shopping-cart ml-2" />
+            </RNavLink>
+          </RNavItem>
+          <RNavItem>
+            <RNavLink href="#" className="nav-link">
+              Revisión de reseñas de productos
+              <i className="fas fa-star ml-2" />
+            </RNavLink>
+          </RNavItem>
+        </RNav>
+      </Navbar.Collapse>
+    </RNavbar>
   );
 }
+
 
 export default MenuOpciones;
--- src/Components/menuvertical.jsx
+++ src/Components/menuvertical.jsx
@@ -2,67 +2,44 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import styled from 'styled-components';
-
-const MenuContainer = styled.div`
-  background-color: #333;
-  padding: 20px;
-  border-radius: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-  width: 120px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const MenuItem = styled.div`
-  padding: 15px;
-  border-bottom: 1px solid #444;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  cursor: pointer;
-  :hover {
-    background-color: #444;
-  }
-`;
-
-const MenuLink = styled.a`
-  text-decoration: none;
-  color: #fff;
-  font-size: 16px;
-  font-weight: bold;
-  :hover {
-    color: #ccc;
-  }
-`;
-
-const Icon = styled.i`
-  font-size: 18px;
-  margin-left: 10px;
-  color: #fff;
-`;
+import { Navbar as RNavbar, Nav as RNav, NavItem as RNavItem, NavLink as RNavLink} from 'react-bootstrap';
 
 function MenuVertical() {
   return (
-    <MenuContainer>
-      <MenuItem>
-        <MenuLink href="#">Productos</MenuLink>
-      </MenuItem>
-      <MenuItem>
-        <MenuLink href="#">Categorías</MenuLink>
-      </MenuItem>
-      <MenuItem>
-        <MenuLink href="#">Usuarios</MenuLink>
-      </MenuItem>
-      <MenuItem>
-        <MenuLink href="#">Marcas</MenuLink>
-      </MenuItem>
-    </MenuContainer>
+    <RNavbar fixed="top" bg="light" expand="lg">
+      <Navbar.Brand href="#home">Mi Aplicación</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <RNav className="mr-auto">
+          <RNavItem>
+            <RNavLink href="#" className="nav-link">
+            Productos
+            </RNavLink>
+          </RNavItem>
+
+          <RNavItem>
+            <RNavLink href="#" className="nav-link">
+              Categorías
+            </RNavLink>
+          </RNavItem>
+          
+          <RNavItem>
+            <RNavLink href="#" className="nav-link">
+              Usuarios
+            </RNavLink>
+          </RNavItem>
+          
+          <RNavItem>
+            <RNavLink href="#" className="nav-link">
+              Marcas
+            </RNavLink>
+          </RNavItem>
+          
+        </RNav>
+      </Navbar.Collapse>
+    </RNavbar>
   );
-
-
-  
 }
+
 
 export default MenuVertical;
