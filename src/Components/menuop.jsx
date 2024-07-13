135a8a19c75030165916a8b5092cf7b1879d4320
Angel Dario Arreola Lucio <20151604@aguascalientes.tecnm.mx>
2024-07-13 03:49:25 -0600

    Modificación de componentes

--- src/Components/menuop.jsx
+++ src/Components/menuop.jsx
@@ -2,32 +2,28 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import React from 'react';
-import { Navbar as RNavbar, Nav as RNav, NavItem as RNavItem, NavLink as RNavLink} from 'react-bootstrap';
+
 function MenuOpciones() {
   return (
-    <RNavbar fixed="top" bg="light" expand="lg">
-      <Navbar.Brand href="#home">Mi Aplicación</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <RNav className="mr-auto">
-          <RNavItem>
-            <RNavLink href="#" className="nav-link">
-              Órdenes de compra
-              <i className="fas fa-shopping-cart ml-2" />
-            </RNavLink>
-          </RNavItem>
-          <RNavItem>
-            <RNavLink href="#" className="nav-link">
-              Revisión de reseñas de productos
-              <i className="fas fa-star ml-2" />
-            </RNavLink>
-          </RNavItem>
-        </RNav>
-      </Navbar.Collapse>
-    </RNavbar>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            
+            <NavDropdown title="Menú Opciones" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Órdenes de compra</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.2"> Revisión de reseñas de productos</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
+
 
 
 export default MenuOpciones;
--- src/Components/menuvertical.jsx
+++ src/Components/menuvertical.jsx
@@ -2,44 +2,30 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Navbar as RNavbar, Nav as RNav, NavItem as RNavItem, NavLink as RNavLink} from 'react-bootstrap';
 
 function MenuVertical() {
   return (
-    <RNavbar fixed="top" bg="light" expand="lg">
-      <Navbar.Brand href="#home">Mi Aplicación</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <RNav className="mr-auto">
-          <RNavItem>
-            <RNavLink href="#" className="nav-link">
-            Productos
-            </RNavLink>
-          </RNavItem>
-
-          <RNavItem>
-            <RNavLink href="#" className="nav-link">
-              Categorías
-            </RNavLink>
-          </RNavItem>
-          
-          <RNavItem>
-            <RNavLink href="#" className="nav-link">
-              Usuarios
-            </RNavLink>
-          </RNavItem>
-          
-          <RNavItem>
-            <RNavLink href="#" className="nav-link">
-              Marcas
-            </RNavLink>
-          </RNavItem>
-          
-        </RNav>
-      </Navbar.Collapse>
-    </RNavbar>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            
+            <NavDropdown title="Menú vertical" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Productos</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.2">Categorías</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.3">Usuarios</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Marcas</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
-
 
 export default MenuVertical;
